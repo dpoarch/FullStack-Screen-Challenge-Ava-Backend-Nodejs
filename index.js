fe50bcb9c792cbe36ecd47259aaaccd8e374387f
dpoarch <davidpoarch@outlook.com>
2022-02-22 16:38:05 +0800

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: "https://react-node-deployment-dev.herokuapp.com/",
+    origin: "react-node-deployment-dev.herokuapp.com",
     methods: ["GET", "POST"],
     credentials: true,
   },
